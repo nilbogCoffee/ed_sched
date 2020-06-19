e79b760452e2a83f2131a0811281e81c3829cc27
houghtons@moravian.edu <houghtons@moravian.edu>
2020-06-19 15:05:33 -0400

    Made first schedule

--- ed_code2.py
+++ ed_code2.py
@@ -37,6 +37,59 @@ def make_teachers(file_name):
     return teacher_list
 
 
+def check_subject(student, teacher):
+    """Check the student and teacher subjects are equal"""
+    return student.get_subject() == teacher.get_subject()
+
+
+def check_grade(student, teacher):
+    """Check that the teacher grade is in the student's certified grades"""
+    return teacher.get_grade() in student.get_grades()
+
+
+def check_availability(student, teacher):
+    """Check that the teacher's availability is in the student's availability"""
+    return teacher.get_availability() in student.get_availability()
+
+
+def check_school(student, teacher):
+    """Check that the teacher's school is not in the students list of previous schools"""
+    return teacher.get_school() in student.get_experience()
+
+
+def check_transport(student, teacher):
+    """Check that the student can drive or walk"""
+    return student.get_can_drive() or teacher.get_can_walk()
+
+
+def print_sched(student, teacher, transport):
+    """Print the schedule results"""
+    if not transport:
+        print(student.get_name() + " will join " + teacher.get_name() + ". Needs to find a ride.")
+    else:
+         print(student.get_name() + " will join " + teacher.get_name())
+
+
+def matchmaker(students, teachers):
+    """
+    Determines which students are matches with each teacher by each student object attribute
+    """
+    for student in students:
+        for teacher in teachers:
+            subject = check_subject(student, teacher)
+            if subject:
+                grade = check_grade(student, teacher)
+                if grade:
+                    availability = check_availability(student, teacher)
+                    if availability:
+                        school = check_school(student, teacher)
+                        if not school:
+                            transport = check_transport(student, teacher)
+                            print_sched(student, teacher, transport)
+                            teacher.set_availability("N/A")
+                            break
+
+
 def main():
     students = make_students("Tidy_Ed_Data_Students.csv")
     teachers = make_teachers("Tidy_Ed_Data_Teachers.csv")
@@ -46,6 +99,7 @@ def main():
     print()
     for teacher in teachers:
         print(teacher)
+    matchmaker(students, teachers)
 
 if __name__ == '__main__':
     main()
